1fd7276855ea04595aade479dac231a441f26a7b
Luis Navarro <navarrocastroluis@gmail.com>
2023-09-09 00:24:11 -0500

    Render Post

--- index.js
+++ index.js
@@ -1,55 +1,54 @@
 const url = "https://jsonplaceholder.typicode.com";
 
 const getUser = async (id) => {
-    const res = await fetch(`${url}/users?id=${id}`);
-    const user = (await res.json())[0];
+  const res = await fetch(`${url}/users?id=${id}`);
+  const user = (await res.json())[0];
 
-    return user;
+  return user;
 }
 
 const getPosts = async (user) => {
-    const res = await fetch(`${url}/post?userId=${user.id}&_limit=3`)
-    const post = await res.jason();
+  const res = await fetch(`${url}/posts?userId=${user.id}&_limit=3`)
+  const posts = await res.json();
 
-    return post;
+  return posts;
 }
 
-const getCommentsForEachPost = async (post) => {
-    const res = await Promise.all(post.map(post =>
-        fetch(`${url}/coments?postId=${post.id}&_limit=2`)
-    )) 
-    const postComments = await Promise.all(res.map(r => r.json()));
-
-    postComments.forEach((Comments, i) => post[i].Comments = Comments)
+const getCommentsForEachPost = async (posts) => {
+  const res = await Promise.all(posts.map(post => 
+    fetch(`${url}/comments?postId=${post.id}&_limit=2`)  
+  ))
+  const postComments = await Promise.all(res.map(r => r.json()));
+  
+  postComments.forEach((comments, i) => posts[i].comments = comments);
 }
 
-const renderHtml =(user, post) => {
+const renderHtml = (user, posts) => {
     const content = document.getElementById('content');
-    content.innerHTML += `<h3>Post del usuario ${user.email}</h3>`;
+    content.innerHTML += `<h3>Posts del usuario ${user.email}</h3>`;
     
-    post.forEach(post => {
-        content.innerHTML += `
-        <div class = "post">
-            <h4>${post.title}</h4>
-            <p>${post.body}</p>
-            <br>
-            ${post.Comments.map( c => `<p><spam>${c.em,email}:</span>${c.body}</p>`).join('')}
-        </div>
-        `;
+    posts.forEach(post => {
+      content.innerHTML += `
+      <div class="post">
+        <h4>${post.title}</h4>
+        <p>${post.body}</p>
+        <br>
+        ${post.comments.map(c => `<p><span>${c.email}:</span>${c.body}</p>`).join('')}
+      </div>
+      `;
     })
 }
 
 const getBlogContent = async () => {
-    try {
-        const user = await getUser (1);
-        const post = await getPosts (user);
-        const getCommentsForEachPost(post);
+  try {
+    const user = await getUser(1);
+    const posts = await getPosts(user);
+    await getCommentsForEachPost(posts);
 
-        console.log(user);
-        console.log(post);
-    }   catch (err) {
-        console.log(err);
-    }
+    renderHtml(user, posts);
+  } catch (err) {
+    console.log(err);
+  }
 }
 
 getBlogContent();
